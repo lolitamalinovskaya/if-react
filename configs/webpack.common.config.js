40416f2a57f9ac83a77230ce2de7de689f5f50d1
lolitamalinovskaya <lolimalinovskaya@gmail.ru>
2021-08-04 23:57:47 +0300

    I added lesson-35,36. I added search. I created section 'Available Hotels'

--- configs/webpack.common.config.js
+++ configs/webpack.common.config.js
@@ -2,67 +2,64 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, '../dist'),
-        filename: 'app.js'
-    },
-    resolve: {
-        extensions: [
-            '.js',
-            '.jsx'
-        ]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: 'html-loader'
-            },
-            {
-                test: /\.css$/,
-                use:
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'app.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        use:
                     [
-                        'style-loader',
-                        'css-loader'
-                    ]
-            },
-            {
-                test: /\.(js|jsx)$/,
-                use: 'babel-loader',
-                include: path.resolve(__dirname, '../src'),
-            },
-            {
-                test: /\.(png|jp(e*)g|svg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'svg/[hash]-[name].[ext]',
-                        },
-                    },
-                ],
-               /* test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]*/
-            },
-            {
-                test: /\.(woff|ttf|eot|svg)(\?v=[a-z0-9]\.[a-z0-9]\.[a-z0-9])?$/,
-                use: [{
-                    loader: 'url-loader?limit=100000',
-                    options: {
-                        outputPath: 'fonts/',
-                        name: '[name].[ext]',
-                    }
-                }]
-            }
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            inject: true,
-            template: './public/index.html'
-        })
-    ]
+                      'style-loader',
+                      'css-loader',
+                    ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+        include: path.resolve(__dirname, '../src'),
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.txt$/,
+        use:
+          'raw-loader',
+      },
+      {
+        test: /\.(woff|ttf|eot)(\?v=[a-z0-9]\.[a-z0-9]\.[a-z0-9])?$/,
+        use: [{
+          loader: 'url-loader?limit=100000',
+          options: {
+            outputPath: 'fonts/',
+            name: '[name].[ext]',
+          },
+        }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+    }),
+  ],
 };
--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
-import React from "react";
-import HeaderGuestLoves from "./HeaderGuestLoves";
-import MainGuestLoves from "./MainGuestLoves";
-
-
+import React from 'react';
+import HeaderGuestLoves from './HeaderGuestLoves';
+import MainGuestLoves from './MainGuestLoves';
+import TopSection from './TopSection';
 
 function App() {
-    return (
-        <section className="homes_guests_loves col-14 col-md-14">
-            <div className="homes_guests_loves_container col-md-14">
-                <HeaderGuestLoves />
-                <MainGuestLoves/>
-            </div>
-        </section>
-    );
+  return (
+    <>
+      <TopSection />
+      <section className="homes_guests_loves col-14 col-md-14">
+        <div className="homes_guests_loves_container col-md-14">
+          <HeaderGuestLoves />
+          <MainGuestLoves />
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
